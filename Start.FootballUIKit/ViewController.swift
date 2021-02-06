9f4aef1164be7c1ca924be32c2456534de0f74e7
Sosin Vitalii <375693@mail.ru>
2021-02-06 18:04:35 +0300

    Initial Commit

---
+++ Start.FootballUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Start.FootballUIKit
+//
+//  Created by Vitalii Sosin on 06.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
